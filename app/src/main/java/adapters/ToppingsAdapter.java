24fea87cc629b77c3d005e680827aabcbe84ed68
elztsang <114364679+elztsang@users.noreply.github.com>
2024-12-01 16:15:28 -0500

    init adapter for recycle view and random xml changes???

---
+++ app/src/main/java/adapters/ToppingsAdapter.java
@@ -0,0 +1,109 @@
+package adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+//import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import pizzaria.Topping;
+
+/**
+ * This is an Adapter class to be used to instantiate an adapter for the RecyclerView.
+ * Must extend RecyclerView.Adapter, which will enforce you to implement 3 methods:
+ *      1. onCreateViewHolder, 2. onBindViewHolder, and 3. getItemCount
+ *
+ * You must use the data type <thisClassName.yourHolderName>, in this example
+ * <ItemAdapter.ItemHolder>. This will enforce you to define a constructor for the
+ * ItemAdapter and an inner class ItemsHolder (a static class)
+ * The ItemsHolder class must extend RecyclerView.ViewHolder. In the constructor of this class,
+ * you do something similar to the onCreate() method in an Activity.
+ * @author Lily Chang
+ */
+class ToppingsAdapter extends RecyclerView.Adapter<ToppingsAdapter.ToppingsHolder>{
+    private static final int MAX_SELECTION = 7;
+    private Context context; //need the context to inflate the layout
+    private ArrayList<Topping> toppings; //need the data binding to each row of RecyclerView
+
+    private final ArrayList<Topping> selectedToppings = new ArrayList<>();
+
+    public ToppingsAdapter(Context context, ArrayList<Topping> toppings, ArrayList<Topping> preselectedToppings) { //TODO: add a parameter for preselected toppings?
+        this.context = context;
+        this.toppings = toppings;
+        selectedToppings.addAll(preselectedToppings);
+    }
+
+    /**
+     * This method will inflate the row layout for the items in the RecyclerView
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    @NonNull
+    @Override
+    public ToppingsHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //inflate the row layout for the items
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.row_view, parent, false);
+        return new ToppingsHolder(view);
+    }
+
+    /**
+     * Assign data values for each row according to their "position" (index) when the item becomes
+     * visible on the screen.
+     * @param holder the instance of ItemsHolder
+     * @param position the index of the item in the list of items
+     */
+    @Override
+    public void onBindViewHolder(@NonNull ToppingsHolder holder, int position) {
+        Topping topping = toppings.get(position);
+
+        holder.textView.setText(topping.name());
+        holder.checkBox.setChecked(selectedToppings.contains(topping));
+        holder.checkBox.setEnabled(selectedToppings.contains(topping) || selectedToppings.size() < MAX_SELECTION);
+
+        holder.itemView.setOnClickListener(v -> {
+            if (holder.checkBox.isEnabled()) {
+                if (holder.checkBox.isChecked()) {
+                    selectedToppings.remove(topping);
+                } else {
+                    if (selectedToppings.size() < MAX_SELECTION) {
+                        selectedToppings.add(topping);
+                    }
+                }
+                notifyDataSetChanged();
+            }
+        });
+    }
+
+    /**
+     * Get the number of items in the ArrayList.
+     * @return the number of items in the list.
+     */
+    @Override
+    public int getItemCount() {
+        return toppings.size(); //number of MenuItem in the array list.
+    }
+
+    /**
+     * Get the views from the row layout file, similar to the onCreate() method.
+     */
+    public static class ToppingsHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+        CheckBox checkBox;
+
+        public ToppingsHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.itemText);
+            checkBox = itemView.findViewById(R.id.itemCheckBox);
+        }
+    }
+}
+
--- app/src/main/java/com/example/androidpizzaria/AddPizzaActivity.java
+++ app/src/main/java/com/example/androidpizzaria/AddPizzaActivity.java
@@ -2,17 +2,27 @@ package com.example.androidpizzaria;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.ListView;
+import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.databinding.ObservableArrayList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import pizzaria.PizzaFactory;
 import pizzaria.Size;
+import pizzaria.Topping;
 
 public class AddPizzaActivity extends AppCompatActivity{
     //id constants
@@ -20,39 +30,59 @@ public class AddPizzaActivity extends AppCompatActivity{
 //    private static final int RBMEDIUM = R.id.rb_medium;
 //    private static final int RBLARGE = R.id.rb_large;
 
+    private final List<ToggleButton> toggleButtons = new ArrayList<>();
 
     Singleton singleton = Singleton.getInstance();
     Button bt_addPizza;
-    RecyclerView rv_pizzaOptions;
+    RecyclerView rv_toppingOptions;
     RadioButton rb_small, rb_medium, rb_large;
     RadioGroup rg_size;
-    Switch sw_byo;
+//    Switch sw_byo;
+    Spinner sp_pizzaOptions;
+    ToggleButton tb_chicago;
+    ToggleButton tb_ny;
 
     private Size selectedSize;
     private PizzaFactory pizzaStyle;
     private boolean isBYO;
+    ObservableArrayList<Topping> toppingOptions; //might not need
+    //might need another array for "selected toppings"; premade pizzas will set this automatically, byo will get to choose
 
+    //TODO:
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addpizza_view);
         findID();
+        toggleButtons.add(tb_chicago);
+        toggleButtons.add(tb_ny);
+        initToppingOptions();   //need a listener for pizza type?
         initClickListeners();
+        initToggleListener();
         initSizeListener();
         //initPizzaOptionListener(); //listener for non-byo
-        initSwitchListener();
+//        initSwitchListener();
+    }
+
+    private void initToppingOptions(){
+        toppingOptions = new ObservableArrayList<>();
+        Collections.addAll(toppingOptions, Topping.values());
+
     }
 
     private void findID() {
         //set all buttons n stuff here
-        bt_addPizza = findViewById(R.id.bt_addPizza);
-        rv_pizzaOptions = findViewById(R.id.rv_pizzaOptions);
-        rg_size = findViewById(R.id.rg_size);
-        rb_small = findViewById(R.id.rb_small);
-        rb_medium = findViewById(R.id.rb_medium);
-        rb_large = findViewById(R.id.rb_large);
-        sw_byo = findViewById(R.id.sw_byo);
+        bt_addPizza = bt_addPizza.findViewById(R.id.bt_addPizza);
+        rv_toppingOptions = rv_toppingOptions.findViewById(R.id.rv_toppingOptions); // need to fix issue here
+        rg_size = rg_size.findViewById(R.id.rg_size);
+        rb_small = rb_small.findViewById(R.id.rb_small);
+        rb_medium = rb_medium.findViewById(R.id.rb_medium);
+        rb_large = rb_large.findViewById(R.id.rb_large);
+        sp_pizzaOptions = sp_pizzaOptions.findViewById(R.id.sp_pizzaOptions);
+        tb_chicago = tb_chicago.findViewById(R.id.tb_chicago);
+        tb_ny = tb_chicago.findViewById(R.id.tb_ny);
+//        sw_byo = findViewById(R.id.sw_byo);
     }
 
     private void initClickListeners() {
@@ -64,16 +94,16 @@ public class AddPizzaActivity extends AppCompatActivity{
         });
     }
 
-    private void initSwitchListener() {
-        sw_byo.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                //change flag to check pizza type
-                isBYO = true;
-            } else {
-                isBYO = false;
-            }
-        });
-    }
+//    private void initSwitchListener() {
+//        sw_byo.setOnCheckedChangeListener((buttonView, isChecked) -> {
+//            if (isChecked) {
+//                //change flag to check pizza type
+//                isBYO = true;
+//            } else {
+//                isBYO = false;
+//            }
+//        });
+//    }
     private void initSizeListener() {
         rg_size.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -87,9 +117,23 @@ public class AddPizzaActivity extends AppCompatActivity{
                 } else if (checkedId == rb_large.getId()) {
                     selectedSize = Size.LARGE;
                 } else {
-                    //print some error somewhere
+                    //TODO: make this a popup instead (forgot the name of it)
                     System.out.println("please select a size");
                 }
+            }
+        });
+    }
+
+    private void initToggleListener() {
+        tb_chicago.setOnClickListener(v -> {
+            if (tb_chicago.isChecked()) {
+                tb_ny.setChecked(false);
+            }
+        });
+
+        tb_ny.setOnClickListener(v -> {
+            if (tb_ny.isChecked()) {
+                tb_chicago.setChecked(false);
             }
         });
     }
@@ -113,7 +157,7 @@ public class AddPizzaActivity extends AppCompatActivity{
     }
 
     //todo: method to populate recycler view
-    private void populateRecyclerView() {
+    private void populateRecyclerView() { // i think we will use this for toppings
 
     }
 }
